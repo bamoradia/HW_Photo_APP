e82a92bb3228072c2e3572d6f9b2a81af3e18dd6
Badal Moradia <badal@i360llc.com>
2018-07-17 18:36:08 -0500

    added styling

--- public/js/app.js
+++ public/js/app.js
@@ -6,16 +6,18 @@ console.log('Javascript is linked');
 
 //add event listener for when use clicks out of the input form
 //checks if both password fields are the same in order to enable submit button
-$('input').on('blur', (event) => {
+$('input').on('keyup', (event) => {
 	$password1 = $('#pass');
 	$password2 = $('#checkpass');
-	console.log($password1.val());
-	console.log($password2.val());	
+	//console.log($password1.val());
+	//console.log($password2.val());	
 
 	if($password1.val() !== $password2.val()) {
 		$('button').prop('disabled', true)
+		$('button').css('cursor', 'not-allowed')
 	} else {
 		$('button').prop('disabled', false)
+		$('button').css('cursor', 'pointer')
 	}
 
 })
